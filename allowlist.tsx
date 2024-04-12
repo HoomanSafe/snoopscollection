86533ad89262445c78ddaee4713d6d6bec53309f
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-04-12 20:36:48 +0800

    Add files via upload

---
+++ allowlist.tsx
@@ -0,0 +1,175 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
+export const allowLists = new Map<string, Array<string>> ([
+    ["TOKEN",
+    [
+      "3SDBsD3zkEuVvmekUFVK1HZZh6jxkzdGZ8RyWqPx2Cih",
+      "BX6YFnocJ6xMiFHWm8kzDBJJgV1ux2t9RjyHovTjm1u8",
+      "DvZkRsxsXxF7g2J6UtTqd8QRqnMcEJpkk5K8wd9JNDiS",
+      "AukkkjDsoxe4tcxVp7RHwyQ3HZdbjxdZMmBC67uQ2oiU",
+      "EUMt5NJs5cf5mpYZdVSPKHTFTUxZpmneX3TABFGKPu6G",
+      "DCfkkrkB6JkxT4UvDXLdHmtBi8kbaFeaCSKvUjS2mZZu",
+      "FvvcqzzardP4xWntuGJHL34t1Br7LDunP36EMUcFAxK3",
+      "Di4PXNtmW2FSx8saAgFbqWPvUDbnLe1GPB87sEUeXPZJ",
+      "DBjDJ1NBtfPTBhESZnjzU5HbhrHEaVYxJM3KZsJfBS9P",
+      "4X5GZcKZjeA3fChZPGspd3LCCLLk6DQu6cgtNApypL5V",
+      "EK9dfkNjeeLUiz3iPvqL4TqkWFBtrCaLWU1tJ9Z2eU8K",
+      "Aygv5tJkfrwNFqJhnRcLdiMGdex2JFDeRaP9MAyBVSy7",
+      "H7BAURKxSTAVqf9F1SfJcbdUyQdehTToBGMgWCuvHM6M",
+      "GvxwrY7r76wg7WuZ6XknGjwNJ5mYckWT8BNTMmrniXyR",
+      "FdRaf1BebH4ShdTazLygALsuU6dd9FSz1tro8tvEamoR",
+      "5L1uHcSy1FjU6yqmTawwnpz3pRK5nu8i8tsbE31eFfiV",
+      "ABN1x5v3rbm72nZGrKncyXYcPFuqKKsCMDPezTTxZbH3",
+      "Hk7Y8LgLYv5aUNRQzQ3g92p23BgTMx5Wo3aeadqpm1h5",
+      "B3Jr4hjz8bGKPAba5tvEq513DkscKqQ5yfTTMu2sVK4k",
+      "74q9QSy9Uis3AQqTPB8ocR9dhZCDoUzUKp4tEm6vkCJe",
+      "4tWdXVDF7bjdntF53VBLCmCnkrrWpYQ2mt3i51BPZe8Q",
+      "Bvrx7m3wa5xvypGDXsC2MfZD9TEjCPNVpzUndHiESahn",
+      "5pxz7y62Lt4RPtBv4awiTCEoAM1V2FMnRXyZUEmUGma3",
+      "HKXQMqkSShXGjAXQfcMdy75mjU5fxFgRgZfKGcY1MZ7g",
+      "5eTMNvf5K1Yu7h8X6qBmuk7D4LS3o6CW5Wxcq2QM9Xzk",
+      "C65TktuuUV1VAdkTb2ZV93id5eSdLzJ4jjdBEmgS8xHH",
+      "22W8k8NzJ51SqX2uzRLEiyr2eM87q3XkoXyUDx54AwPM",
+      "F5WfVeALjgkf1zG67hizLUp7PPGez7ZPcuPzenMk9xPN",
+      "FDYinKbeZNnynKXSqgrYWEG7ZZ643mwTE1wWghsxbAyo",
+      "9Etm9mVN6sjxJg2L5vDFxYvAp27S685nd5Kzyvh82pYS",
+      "89Mqzp3puSt8fprhuei6MQJX8fpXnU8NzFihfgm7hGU2",
+      "68RVfLXgboKEXpovaK3KJY7r782KAK6vqLq1YdyMPeYa",
+      "8EK4ytDzvJpQW25inhkNRkWay5emYyavxVKDntYm6ZGc",
+      "4G463juEkGTcaGKevdMXW85EARJqfo5CE7h4sJzPrkGe",
+      "DD2mLFe76d8VmSc4DUNy9u1Ei6wwQZ24mKsYJSdhshE8",
+      "BisxeuSfUV81d8DufJHNfpynyp4mYkPoXWSQAe6PGTVd",
+      "Bx9fgsPUJ5qS4bCzstmY4256WCVuPn4ZdCEnzD93soLs",
+      "EQeQFqhKQFBY439N9JDg7NomkAmobfXuFY5qxmiKsRHG",
+      "6C88DrL15wGrWpU535FopuybXgDCuosvZj74HUudj6kM",
+      "EKtah16bkAYxAbK3iFksxvjXLsKSZtnbNRwu7QKWeKsT",
+      "4jvCv6DEKFWmkpDhH2xos1MHJvky7QZSvjkL4U5sasKF",
+      "B3CNn4YLKTE7szfgeNVFESp2PEcHzhRV2zjxtj9UYMev",
+      "9KmZvGcPMjUZTzpizhNzMmzoBm55C5WqAMDfeHtqcTyd",
+      "GVtxc14onDhoHzfS4AtRoa9udPjDExxhPfLtZ39JZrau",
+      "4hpPqkUdamPNcpcFkXSpgzsJk5Z6c8hkKRyX1Dj98nDw",
+      "4QCtpAbH5RjgunRgg1ZAZjJAnfyfpLb1g2G6cCEe58E2",
+      '3aEnNkLQoMhgGHd6uRBeQNRm4PpzX8PHpmjodgZ3cRN7',
+      "4JSayeUJ51YiWjQ7Kvwp1rTWGmjyZwe1WT8n36kD3bjB",
+      "HYnutXF19nrYyK6zs7jd9iodb7psN9tbPaq5mr44H5oy",
+      "HjaHZ9XZtcePUfp7S9TbtcbE7DtvPSCuWc7kyJyfd8E",
+      "3ksGnPRVTkBJgGLX1H1sPTUL4rLWKJdv5mGRfLf8uNB5",
+      "CkfP4SKcmf1PJAiuBPGXA31PnrB4BhgSBngiLMeWVmJG",
+    ]],
+    ["WL1",
+    [
+      "7Phvdn8XmmLgZV4i5nqWEeJV4nUsFwA5H9GxdycHdoDB",
+      "9RmURGxLsNWXhYe6dQr89hRGqsN5gFgPkVSCfSbiJ8u7",
+      "CSzprfjM2UTFmjkdtHjV2o4NFHaaFA3yqi3DaWPFNDfY",
+      "EPnCHUGxnfDjKMXfFuHuWnm5HdfPsR8kqDTVmHNk6oxg",
+      "GBDqDSXg7JZY7mjTA8obVFo8LkEa6PLhsicac1ukSkpc",
+      "UEMiHjwy8DhGvpnFVrQ6c4ZJT7YTeR1Vz8ebi9XfXWp",
+      "hYA7LYTuKK9HAQVjsTTpQmoybPKHpKH2E9YiwDHKShX",
+      "5UXSvQv5ZYo4zxnsZUdzYE7mPaYP5TwTazUa8C344xJd",
+      "3aEnNkLQoMhgGHd6uRBeQNRm4PpzX8PHpmjodgZ3cRN7",
+
+      
+    ]],
+    ["WL2",
+    [
+      "C45vvu2Uv8LB9x5fcyyKh1pysFNBAQfimGZeJfr5r9ec",
+      "6YT87vETBzCF8jBdPSVs3kPCrKbtERufLsCu1r9KnRaU",
+      "Bvrx7m3wa5xvypGDXsC2MfZD9TEjCPNVpzUndHiESahn",
+      "4jvCv6DEKFWmkpDhH2xos1MHJvky7QZSvjkL4U5sasKF",
+      
+    ]],
+    ["WL3",
+    [
+      "DMrc2xxCTHK8vCfkWVrLQzfE5iAZcM5pgQtLmdWkWFRg",
+      "HwPWAq2FLJJQJgvEPBfwWDszaVwjB1pH5zPh3JkNoy3a",
+      
+    ]],
+    ["WL4",
+    [
+      "6YT87vETBzCF8jBdPSVs3kPCrKbtERufLsCu1r9KnRaU",
+      
+    ]],
+    
+    ["WL5",
+    [
+      
+      "Ac3msUwNbX9TWLJ6Th4himF4WEo1J2mgvoGTu9R1pJkq",
+      "6YT87vETBzCF8jBdPSVs3kPCrKbtERufLsCu1r9KnRaU",
+      "97Bgt5bxcVR7G13EDfTJQe2kH7NCQAzHhF9ryPX9pTGp",
+      "5EravtRmM2ErtJ6JAKRGxqPEy9C7QnUS6pmntszVVwmS",
+      "G1bVuVH7NWQzpuhtSiLgcvdVZunotXrYyVw394pXyTSo",
+      "FgQ1kkGmYNrJXkNW7DnkTqHJNie51TpsBfj4JaPZV2EE",
+      "Fk1F9QDyCag7R73GVABcC8M53YP5YcZpLw6ejDdH3oWg",
+      "ANGtY3z1MQ2MXy62gEoYJTDoFmkZMW67FHRhWAiA1Bt5",
+      "HAUYjYEAdDS7f16mc9jrvwPy4Ls1CYoq4HSLFJfzMzSu",
+      "9CLiVvyyHNozNeFnPG2j1ACipVLUDGhs5AFKiGijHeur",
+      "9kcCrVaLJBXsjtU5fsY2Qk2uPekWjAbDfJMEMLaKoazK",
+      "AZZKLHNYmwLUed1PcEJF9Cve1FZEwbmuwJAjGMRgpWLS",
+     
+      "2SFdu5xnNbmRUn3wxzoCKhECXYz7Qam2W2Fw74nAfT7h",
+      "4NmsuLdU8SYVtaz23QNp3M7zCAKSgvutBXHjVLC6jVry",
+      "83p7LLwSp4Jnd78p6jCYbSFwkNY7XfMfRuKEECDVrKeY",
+      "9oz4VXEDLWxGt7fTa8P1Qz7YE8E5jPqsJEy7Jg8scdNm",
+      "4xf31Kc4XCprhNVhbtqtE5UAEazT4qSxBMEWuR8BRTH9",
+      "3aEnNkLQoMhgGHd6uRBeQNRm4PpzX8PHpmjodgZ3cRN7",
+      "53gQPhGQNWYd5V4f39DpG6CEJwqCXzoRAbMJar3ddBp9",
+      
+      "CVmPD27wkXXUY5afp9fURogRwgGgmpqWLroZBYVgstSH",
+      
+
+
+      
+    ]],
+    ["WL6",
+    [
+      "3CjRLR85AxwLTcxWsfEBLUvE8VVgrzzr8x6KNtLML4Bd",
+      
+    ]],
+    ["WL10",
+    [
+      "HDhjZcALcUZyeqht7om3yR15wyAWwkAPhTqVBLDxx1ss",
+      
+      "3SDBsD3zkEuVvmekUFVK1HZZh6jxkzdGZ8RyWqPx2Cih",
+      "Bvrx7m3wa5xvypGDXsC2MfZD9TEjCPNVpzUndHiESahn",
+      "AukkkjDsoxe4tcxVp7RHwyQ3HZdbjxdZMmBC67uQ2oiU",
+      "5h8RqEugdvGe7KLj27ABsuTiyCTem51h3x9vtwmETSY6",
+      "4tWdXVDF7bjdntF53VBLCmCnkrrWpYQ2mt3i51BPZe8Q",
+      "5pntj1um5wsf4EvKf5fVN4BxVhNnx1sHLi8VGUvLLVNg",
+      "6YT87vETBzCF8jBdPSVs3kPCrKbtERufLsCu1r9KnRaU",
+      "5EravtRmM2ErtJ6JAKRGxqPEy9C7QnUS6pmntszVVwmS",
+      
+    ]],
+    ["WL11",
+    [
+      "2Jk3EJp325bFM1U6KsAFceTWXUkb9K9YkRTcm8AuHJvT",
+      
+    ]],
+    ["WL15",
+    [
+      "Di4PXNtmW2FSx8saAgFbqWPvUDbnLe1GPB87sEUeXPZJ",
+      "Di4PXNtmW2FSx8saAgFbqWPvUDbnLe1GPB87sEUeXPZJ",
+      
+    ]],
+    
+    ["WLMA3",
+    [
+      "5pxz7y62Lt4RPtBv4awiTCEoAM1V2FMnRXyZUEmUGma3",
+      
+    ]],
+    ["WL863",
+    [
+      "9S3Jjd7dHSdPKirPhT3kkuYxDAyNmiKNHdNaHNDrMsd1",
+      "57GExhFJjnkyJdZ6q5Hwwp2TnWS1ajyyxLNUYbAPdC7K",
+      
+    ]],
+    ["WLDIS",
+    [
+      "DvZkRsxsXxF7g2J6UtTqd8QRqnMcEJpkk5K8wd9JNDiS",
+      
+    ]],
+    ["WLD8E",
+    [
+      "HjaHZ9XZtcePUfp7S9TbtcbE7DtvPSCuWc7kyJyfd8E",
+      
+    ]],
+]);
---
+++ settings.tsx
@@ -0,0 +1,26 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "TOKEN", mintText: "Hold 500K $SNOOPY on Snapshot", buttonLabel: "Mint now!", header: "Snapshot $SNOOPY max 10Nfts" },
+  { label: "WL1", mintText: "1 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Twilight" },
+  { label: "WL2", mintText: "2 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Blissful Haven" },
+  { label: "WL3", mintText: "3 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Zenith" },
+  { label: "WL4", mintText: "4 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Oasis Retreat" },
+  { label: "WL5", mintText: "5 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Tranquility" },
+  { label: "WL6", mintText: "6 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Solstice" },
+  { label: "WL10", mintText: "10 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Radiant Realm" },
+  { label: "WL11", mintText: "11 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Harmonics" },
+  { label: "WL15", mintText: "15 per Wallet - Free", buttonLabel: "Mint now!", header: "Phase Serenity Springs" },
+  { label: "WLMA3", mintText: "Mint - Free", buttonLabel: "Mint now!", header: "Phase VIP " },
+  { label: "WL863", mintText: "Mint - Free", buttonLabel: "Mint now!", header: "Phase Team" },
+  { label: "WLDIS", mintText: "Mint Phase - Free", buttonLabel: "Mint now!", header: "Phase Celestial Haven" },
+  { label: "WLD8E", mintText: "Mint Phase - Free", buttonLabel: "Mint now!", header: "Phase Whispering Woods" },
+
+  { label: "public", mintText: "Mint Snoops", buttonLabel: "Mint now!", header: "Public"}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://arweave.net/nGJROl6bzaLjmJvX3kn-pAX8VLkyYp99USFPAa_TGfY?ext=png";
+
+//website title
+export const headerText = "Snoops Collection Dapp";
